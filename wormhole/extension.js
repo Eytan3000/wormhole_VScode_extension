bcfa95facf20bd5130574fb958c07b10b05bdb99
Eytan3000 <eytankrief@gmail.com>
2024-04-25 14:01:31 +0300

    before changeing goToSave to include file path

--- wormhole/extension.js
+++ wormhole/extension.js
@@ -1,43 +1,80 @@
 const vscode = require('vscode');
 
-
-// Function to go to a specific line
-function goToLine(line = 500) {
+// Function to save the current line number
+async function saveCurrentLine() {
   const editor = vscode.window.activeTextEditor;
   if (editor) {
-    const newPosition = new vscode.Position(line, 0);
-    const newSelection = new vscode.Selection(newPosition, newPosition);
-    editor.revealRange(
-      new vscode.Range(newPosition, newPosition),
-      vscode.TextEditorRevealType.InCenter
+    const currentPosition = editor.selection.active.line;
+    console.log('currentPosition: ', currentPosition);
+
+    const currentFilePath = editor.document.uri.fsPath; // Get the file path of the active document
+    const fileInfo = {
+      filePath: currentFilePath,
+      line: currentPosition,
+    };
+    console.log('File info:', fileInfo);
+
+    // // Update the configuration
+    // await vscode.workspace
+    //   .getConfiguration()
+    //   .update('savedLine', currentPosition, vscode.ConfigurationTarget.Global);
+
+    // Update the configuration
+    await vscode.workspace
+      .getConfiguration()
+      .update('savedFileInfo', fileInfo, vscode.ConfigurationTarget.Global);
+
+
+	  const savedPos = vscode.workspace.getConfiguration().get('savedFileInfo', -1); // Default to -1 if the key is not found
+	  console.log('saved filePath:', savedPos.filePath);
+	  console.log('saved line:', savedPos.line);
+	  
+    vscode.window.showInformationMessage(
+      `Current Position saved!`
     );
-    editor.selection = newSelection;
   }
 }
+
+// Function to go to the saved line
+function goToSavedLine() {
+  const savedLine = vscode.workspace.getConfiguration().get('savedLine', -1); // Default to -1 if the key is not found
+
+  console.log('savedLine: ', savedLine);
+
+  if (savedLine !== -1) {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const newPosition = new vscode.Position(savedLine, 0);
+      const newSelection = new vscode.Selection(newPosition, newPosition);
+      editor.revealRange(
+        new vscode.Range(newPosition, newPosition),
+        vscode.TextEditorRevealType.InCenter
+      );
+      editor.selection = newSelection;
+    }
+  } else {
+    vscode.window.showInformationMessage('No line saved.');
+  }
+}
+
 //---------------------------------------
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "wormhole" is now active!');
 
-  // let disposable = vscode.commands.registerCommand('wormhole.helloWorld', function () {
-  // 	// The code you place here will be executed every time your command is executed
-
-  // 	// Display a message box to the user
-  // 	vscode.window.showInformationMessage('Hello World from wormhole!');
-  // });
-
-  // context.subscriptions.push(disposable);
-
-  let disposable = vscode.commands.registerCommand(
-    'wormhole.helloWorld',
+  let disposable1 = vscode.commands.registerCommand(
+    'wormhole.saveCurrentLine',
     () => {
-      //   return printLowestVisibleLine();
-      //   jumpToLine();
-      goToLine();
+      saveCurrentLine();
     }
   );
-  context.subscriptions.push(disposable);
+  let disposable2 = vscode.commands.registerCommand(
+    'wormhole.goToSavedLine',
+    () => {
+      goToSavedLine();
+    }
+  );
+  context.subscriptions.push(disposable1);
+  context.subscriptions.push(disposable2);
 }
 
 // This method is called when your extension is deactivated
